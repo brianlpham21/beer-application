0c7a2edafce8014758de45b511628a86bfa27012
Brian Pham <brianlpham21@gmail.com>
2018-01-16 17:05:17 -0800

    added travis file

--- server.js
+++ server.js
@@ -3,36 +3,10 @@ const app = express();
 
 app.use(express.static('public'));
 
-let server;
-
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
-    });
-  });
-}
-
-function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
-}
-
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  app.listen(process.env.PORT || 8080, function () {
+    console.info(`App listening on ${this.address().port}`);
+  });
 };
 
-module.exports = {app, runServer, closeServer};
-//
+module.exports = app;
--- test/test-server.js
+++ test/test-server.js
@@ -2,31 +2,18 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const app = require('../server');
 
 const expect = chai.expect;
 
-const {app, runServer, closeServer} = require('../server');
-
 chai.use(chaiHttp);
 
-describe('Beer API Resource', function() {
-
-  before(function() {
-    runServer();
+describe('Index Page', function() {
+  it('should return HTML and a 200 status code', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
   });
-
-  after(function() {
-    closeServer();
-  });
-
-  describe('GET Endpoint', function() {
-    it('should return HTML and a 200 status code', function() {
-      return chai.request(app)
-        .get('/')
-        .then(function(res) {
-          expect(res).to.have.status(200);
-        })
-    });
-  });
-  
 });
