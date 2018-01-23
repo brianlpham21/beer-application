11ec73c041ef8979f4d0dd8fc5c77ee78cf99618
Brian Pham <brianlpham21@gmail.com>
2018-01-22 21:26:39 -0800

    added update request method

--- public/app.js
+++ public/app.js
@@ -34,19 +34,20 @@ $('.search').on('submit', function(event) {
 });
 
 function retrieveJSON(searchTerm, callback) {
-  $.getJSON(API_URL + `/${searchTerm}`, callback);
+  $.getJSON(API_URL + `/beer/${searchTerm}`, callback);
 }
 
 function displayBeerInformation(data) {
-  $('.result').html(
+  $('.result').append(
     `<div>
       <h2 class='search-beer-name'>${data.beerName}</h2>
       <p class='search-beer-type'>${data.beerType}</p>
-      <p><span class='search-brewery-name'>${data.breweryName}</span> - ${data.breweryLocation}</p>
+      <p><span class='search-brewery-name'>${data.breweryName}</span> - <span class='search-brewery-location'>${data.breweryLocation}</span></p>
       <p>ABV: <span class='search-beer-abv'>${data.beerABV}</span></p>
       <p>IBU: <span class='search-beer-ibu'>${data.beerIBU}</span></p>
       <p>Availability: <span class='search-beer-availability'>${data.beerAvailability}</span></p>
       <p>Notes: <span class='search-beer-notes'>${data.beerNotes}</span></p>
+      <p hidden>ID: <span class='search-beer-id'>${data.id}</span></p>
       <button class="edit-button">Edit</button>
       <button class="delete-button">Delete</button>
     </div>`
@@ -112,6 +113,8 @@ $('.add-beer-form').on('submit', function(event) {
   queryTargetBeerIBU.val("");
   queryTargetBeerAvailability.val("");
   queryTargetBeerNotes.val("");
+
+  alert("Beer Added!")
 });
 
 // event listener to delete beer item
@@ -130,10 +133,11 @@ function deleteBeer(data) {
 $('.result').on('click', '.delete-button', function(event) {
   event.preventDefault();
 
-  const queryTargetBeerName = $(event.currentTarget).find('.search-beer-name');
-  const queryTerm = queryTargetBeerName.html();
+  const queryTargetBeerName = $('.search-beer-name').html();
 
-  retrieveJsonAndDelete(queryTerm, deleteBeer);
+  retrieveJsonAndDelete(queryTargetBeerName, deleteBeer);
+
+  alert("Beer Deleted!")
 });
 
 // event listener to edit beer item
@@ -144,29 +148,76 @@ $('.result').on('click', '.edit-button', function(event) {
   const queryTargetBeerName = $('.search-beer-name').html();
   const queryTargetBeerType = $('.search-beer-type').html();
   const queryTargetBreweryName = $('.search-brewery-name').html();
-  const queryTargetBreweryCity = $('.search-brewery-city').html();
-  const queryTargetBreweryState = $('.search-brewery-state').html();
+  const queryTargetBreweryCity = $('.search-brewery-location').html().split(',')[0];
+  const queryTargetBreweryState = $('.search-brewery-location').html().split(',')[1].trim();
   const queryTargetBeerABV = $('.search-beer-abv').html();
   const queryTargetBeerIBU = $('.search-beer-ibu').html();
   const queryTargetBeerAvailability = $('.search-beer-availability').html();
   const queryTargetBeerNotes = $('.search-beer-notes').html();
+  const queryTargetBeerId = $('.search-beer-id').html();
+
+  console.log(queryTargetBeerNotes);
 
   $('.result').html(
     `<div>
       <form class="edit-beer-form">
-        <input type="text" id="edit-beer-name" placeholder="${queryTargetBeerName}" required><br>
-        <input type="text" id="edit-beer-type" placeholder="${queryTargetBeerType}" required><br>
-        <input type="text" id="edit-brewery-name" placeholder="${queryTargetBreweryName}" required required><br>
-        <input type="text" id="edit-brewery-city" placeholder="${queryTargetBreweryCity}"><br>
-        <input type="text" id="edit-brewery-state" placeholder="${queryTargetBreweryState}" required><br>
-        <input type="text" id="edit-beer-abv" placeholder="${queryTargetBeerABV}"><br>
-        <input type="text" id="edit-beer-ibu" placeholder="${queryTargetBeerIBU}"><br>
-        <input type="text" id="edit-beer-availability" placeholder="${queryTargetBeerAvailability}"><br>
-        <input type="text" id="edit-beer-notes" placeholder="${queryTargetBeerNotes}"><br>
+        <input type="text" id="edit-beer-name" value="${queryTargetBeerName}" required><br>
+        <input type="text" id="edit-beer-type" value="${queryTargetBeerType}" required><br>
+        <input type="text" id="edit-brewery-name" value="${queryTargetBreweryName}" required><br>
+        <input type="text" id="edit-brewery-city" value="${queryTargetBreweryCity}" required><br>
+        <input type="text" id="edit-brewery-state" value="${queryTargetBreweryState}" required><br>
+        <input type="text" id="edit-beer-abv" value="${queryTargetBeerABV}"><br>
+        <input type="text" id="edit-beer-ibu" value="${queryTargetBeerIBU}"><br>
+        <input type="text" id="edit-beer-availability" value="${queryTargetBeerAvailability}"><br>
+        <textarea id="edit-beer-notes">${queryTargetBeerNotes}</textarea><br>
+        <input type="text" id="edit-beer-id" value="${queryTargetBeerId}" hidden><br>
         <button type="submit" class='edit-beer-form-submit'>Submit</button>
       </form>
     </div>`
   );
+});
 
-  console.log('Edit Button Pressed!');
+$('.result').on('click', '.edit-beer-form-submit', function(event) {
+  event.preventDefault();
+
+  const queryTargetBeerName = $('#edit-beer-name').val();
+
+  const queryTermBeerName = $('#edit-beer-name').val();
+  const queryTermBeerType = $('#edit-beer-type').val();
+  const queryTermBreweryName = $('#edit-brewery-name').val();
+  const queryTermBreweryCity = $('#edit-brewery-city').val();
+  const queryTermBreweryState = $('#edit-brewery-state').val();
+  const queryTermBeerABV = $('#edit-beer-abv').val();
+  const queryTermBeerIBU = $('#edit-beer-ibu').val();
+  const queryTermBeerAvailability = $('#edit-beer-availability').val();
+  const queryTermBeerNotes = $('#edit-beer-notes').val();
+  const queryTermBeerId = $('#edit-beer-id').val();
+
+  const objectPost = {
+    "id": queryTermBeerId,
+    "beerName": queryTermBeerName,
+    "beerType": queryTermBeerType,
+    "breweryName": queryTermBreweryName,
+    "breweryLocation": {
+      "city": queryTermBreweryCity,
+      "state": queryTermBreweryState
+    },
+    "beerABV": queryTermBeerABV,
+    "beerIBU": queryTermBeerIBU,
+    "beerAvailability": queryTermBeerAvailability,
+    "beerNotes": queryTermBeerNotes
+  };
+
+  $.ajax({
+    type: "PUT",
+    url: API_URL + `/${queryTermBeerId}`,
+    data: JSON.stringify(objectPost),
+    error: function(e) {
+      console.log(e);
+    },
+    dataType: "json",
+    contentType: "application/json"
+  });
+
+  alert("Beer Updated!")
 });
